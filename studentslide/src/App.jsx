de9467d7cb92caaf1bb1e2a237472fc6f5a44f43
Nkanyi-007 <251306@virtualwindow.co.za>
2026-04-30 10:43:07 +0200

    Update App.jsx

--- studentslide/src/App.jsx
+++ studentslide/src/App.jsx
@@ -5,7 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(15)
+  const [count, setCount] = useState(5)
 
   return (
     <>
