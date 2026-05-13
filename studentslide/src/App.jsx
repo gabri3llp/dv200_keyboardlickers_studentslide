dba8d77e7606624980f55c7f0b59079a1dde5904
gabri3llp <251298@virtualwindow.co.za>
2026-05-13 18:43:44 +0200

    Mongo UP AND RUNNING!!!!!
    I have added mongo and gotten it ready, i have also made file structures! No errors so far

--- studentslide/src/App.jsx
+++ studentslide/src/App.jsx
@@ -5,13 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(5)
 
-  return (
-    <>
-      
-    </>
-  )
 }
 
 export default App
